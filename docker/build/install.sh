b5132093fb054e8efa3ded3ae81cffdca64f27d3
EvertBunschoten <evertbun@xs4all.nl>
2024-05-03 12:28:02 +0200

    Added simple test case and regression test files

---
+++ docker/build/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -l 
+
+mkdir "src"
+cd "src"
+git clone https://github.com/EvertBunschoten/test_with_regression.git 
+cd test_with_regression 
---
+++ simple_function.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+x = 1.5 
+
+y = np.sin(x)
+
+print(y)
